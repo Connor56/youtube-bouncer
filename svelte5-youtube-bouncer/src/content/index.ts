8f578105cb505361764be1dd3f2004c5e1e8995e
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-08-12 09:14:18 +0100

    feat: add svelte 5 skeleton to rebuild the chrome extension with
    This method allows for a better developer experience even if it results
    in an untidy development space, in my opinion.

---
+++ svelte5-youtube-bouncer/src/content/index.ts
@@ -0,0 +1,5 @@
+console.log("hi");
+
+export function LargeFile() {
+  return;
+}
---
+++ svelte5-youtube-bouncer/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+      input: {
+        content: "./src/content/index.ts",
+      },
+    },
+  },
+});
